580de7dae3598c11e4d53bdd0aa28494cdd8ff52
Thanh Duong <thanh@trinity-technology.com>
2025-02-12 10:50:49 +0700

    feat: add LoadConfigByKey function for enhanced configuration loading with defaults

--- config/config.go
+++ config/config.go
@@ -55,3 +55,30 @@ func LoadConfig(configName string, configPaths []string, data any) error {
 func GetAppConfig() *Config {
 	return &cfg
 }
+
+func LoadConfigByKey(configName, key string, configPaths []string, data any, defaultValues map[string]any) error {
+	// Set configuration file details
+	viper.SetConfigName(configName)
+	viper.SetConfigType("yaml")
+
+	// Set search paths for config files
+	for _, path := range configPaths {
+		viper.AddConfigPath(path)
+	}
+
+	// set default value
+	for k, v := range defaultValues {
+		viper.SetDefault(k, v)
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	if err := viper.UnmarshalKey(key, data); err != nil {
+		return fmt.Errorf("error unmarshal config: %v", err)
+	}
+	return nil
+}
